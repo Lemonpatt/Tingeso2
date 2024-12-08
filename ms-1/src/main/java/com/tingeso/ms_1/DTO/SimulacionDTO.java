0530feaddf34a44ed1cea33c65f4b0496b082b34
LemonPatt <patricio.briceno@usach.cl>
2024-12-08 03:49:42 -0300

    Cambio a local

--- ms-1/src/main/java/com/tingeso/ms_1/DTO/SimulacionDTO.java
+++ ms-1/src/main/java/com/tingeso/ms_1/DTO/SimulacionDTO.java
@@ -1,12 +1,49 @@
 package com.tingeso.ms_1.DTO;
 
-import jakarta.persistence.Entity;
-import lombok.*;
 
-@Data
+
 public class SimulacionDTO {
     private int valorPropiedad;
     private int plazo;
     private double interes;
     private double financiamiento;
+
+    public SimulacionDTO(int valorPropiedad, int plazo, double interes, double financiamiento) {
+        this.valorPropiedad = valorPropiedad;
+        this.plazo = plazo;
+        this.interes = interes;
+        this.financiamiento = financiamiento;
+    }
+
+    public int getValorPropiedad() {
+        return valorPropiedad;
+    }
+
+    public void setValorPropiedad(int valorPropiedad) {
+        this.valorPropiedad = valorPropiedad;
+    }
+
+    public int getPlazo() {
+        return plazo;
+    }
+
+    public void setPlazo(int plazo) {
+        this.plazo = plazo;
+    }
+
+    public double getInteres() {
+        return interes;
+    }
+
+    public void setInteres(double interes) {
+        this.interes = interes;
+    }
+
+    public double getFinanciamiento() {
+        return financiamiento;
+    }
+
+    public void setFinanciamiento(double financiamiento) {
+        this.financiamiento = financiamiento;
+    }
 }
