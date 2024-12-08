b009032f07969f987a8065fc72ec099881c8cada
LemonPatt <patricio.briceno@usach.cl>
2024-12-08 04:42:36 -0300

    Arreglo CORS

--- ms-2/src/main/java/com/tingeso/ms_2/controllers/UsuarioController.java
+++ ms-2/src/main/java/com/tingeso/ms_2/controllers/UsuarioController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/usuario")
+@RequestMapping("/api/v1/usuario")
 @CrossOrigin("*")
 public class UsuarioController {
 
